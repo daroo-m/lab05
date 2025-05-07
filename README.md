713caf5413909a026982851dc4c6d4f8e5f1e8be
abiduke <abiduke@umich.edu>
2025-05-07 17:49:38 -0400

    added files

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # lab05
 EECS402 Discussion / Lab 05
+
+## Constructors, P2, Strings
---
+++ strings.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+int main() {
+
+	char str1[] = {'h', 'e', 'l', 'l', 'o'};
+	char str2[] = "EECS402";
+	
+	cout << str1 << str2 << endl;
+
+	return 0;
+
+}
